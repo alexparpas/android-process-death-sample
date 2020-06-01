e68dc0f728d25b1b4982c2087f3ad557247f5a49
Alex Parpas <alex.parpas@incrowdsports.com>
2020-06-01 14:40:37 +0100

    Singleton troublemaker fixed.

--- app/src/main/java/com/alexparpas/sample/process/troublemakers/singleton/LoginManager.kt
+++ app/src/main/java/com/alexparpas/sample/process/troublemakers/singleton/LoginManager.kt
@@ -1,8 +1,23 @@
 package com.alexparpas.sample.process.troublemakers.singleton
 
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import com.alexparpas.sample.process.R
+import javax.inject.Singleton
+
+@Singleton
 object LoginManager {
-    var isLoggedIn: Boolean = false
-        private set
+    lateinit var app: Application
+    private const val PREF_IS_LOGGED_IN = "PREF_IS_LOGGED_IN"
+
+    fun init(app: Application) {
+        this.app = app
+    }
+
+    var isLoggedIn: Boolean
+        get() = getSharedPreferences().getBoolean(PREF_IS_LOGGED_IN, false)
+        set(value) = getSharedPreferences().edit().putBoolean(PREF_IS_LOGGED_IN, value).apply()
 
     fun login(callback: () -> Unit) {
         isLoggedIn = true
@@ -18,4 +33,7 @@ object LoginManager {
 
         callback.invoke()
     }
+
+    private fun getSharedPreferences(): SharedPreferences =
+        app.getSharedPreferences(app.resources.getString(R.string.app_name), Context.MODE_PRIVATE)
 }
